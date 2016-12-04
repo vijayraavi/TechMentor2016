c60f550444dbe45c25a52aacc2438dcdd9274045
Jason Helmick <jason-helmick@pluralsight.com>
2016-12-04 14:14:00 -0700

    update

---
+++ DSC/02.LCM/2a.LCM_Settings_Simple.ps1
@@ -0,0 +1,19 @@
+﻿[DSCLocalConfigurationManager()]
+Configuration LCM {	
+
+    Node s1 # <--- Hardcoded Computer Name
+	{
+		Settings # Hit Ctrl-Space for help
+		{
+            ConfigurationMode = 'ApplyAndAutoCorrect'
+            RebootNodeIfNeeded = $true		
+		}
+	}
+}
+
+# Create the Computer.Meta.Mof in folder
+LCM -OutputPath .\
+
+
+
+
