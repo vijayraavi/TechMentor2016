a1ce6db6f8020e86f2d6ce33f2c1ac0cc0f258e2
Jason Helmick <jason-helmick@pluralsight.com>
2016-12-04 14:54:19 -0700

    update

---
+++ DSCPullServer/1.PullServer.ps1
@@ -0,0 +1,38 @@
+﻿Configuration AutoLab {
+
+#region DSC Resources
+    Import-DSCresource -ModuleName PSDesiredStateConfiguration,
+        @{ModuleName="xPSDesiredStateConfiguration";ModuleVersion="5.0.0.0"},
+        @{ModuleName="xActiveDirectory";ModuleVersion="2.14.0.0"},
+        @{ModuleName="xComputerManagement";ModuleVersion="1.8.0.0"},
+        @{ModuleName="xNetworking";ModuleVersion="3.0.0.0"},
+        @{ModuleName="xDhcpServer";ModuleVersion="1.5.0.0"},
+        @{ModuleName='xWindowsUpdate';ModuleVersion = '2.5.0.0'},
+        @{ModuleName='xPendingReboot';ModuleVersion = '0.3.0.0'},
+        @{ModuleName='xADCSDeployment';ModuleVersion = '1.0.0.0'}
+
+#endregion
+#region All Nodes
+    node $NodeName {
+
+        ## Hack to fix DependsOn with hypens "bug" :(
+        foreach ($feature in @(
+                'DNS',                           
+                'AD-Domain-Services',
+                'RSAT-AD-Tools', 
+                'RSAT-AD-PowerShell',
+                'GPMC'
+                #For Gui, might like
+                #'RSAT-DNS-Server',                     
+                #'RSAT-AD-AdminCenter',
+                #'RSAT-ADDS-Tools'
+
+            )) {
+            WindowsFeature $feature.Replace('-','') {
+                Ensure = 'Present';
+                Name = $feature;
+                IncludeAllSubFeature = $False;
+            }
+        } #End foreach
+    }
+}
