aa9b00ead7c1eb9435ba392c3508c827f6d9d8e6
Jason Helmick <jason-helmick@pluralsight.com>
2016-12-05 09:36:56 -0700

    update

--- DSCPullServer/0.Demo.ps1
+++ DSCPullServer/0.Demo.ps1
@@ -1,3 +1,36 @@
 ﻿
 #Pull server
 
+# Let's start with some requirements and documentation
+<#
+1. You will need to install a certificate on the Pull servers - I will show you how
+2. Add a DNS Entry for the Pull Server
+2. You will need module xPSDesiredStateConfiguration and xWebAdministration on the Pull server - I will show you how
+3. You should look at the documentation inside the module xPSDesiredStateConfiguration - I will...you get the idea
+4. You should create YOUR OWN config -- I'll show you mine
+5. Then deploy a pull server - AND test it.
+#>
+
+# Let's get started by installing a certificate on the Pull server
+
+$ComputerName = 'Pullserver' # Enter you target web server
+Invoke-Command -computername $ComputerName {
+    Get-Certificate -template 'WebServer' -url 'https://localhost/ADPolicyProvider_CEP_Kerberos/service.svc/cep' `
+    -CertStoreLocation Cert:\LocalMachine\My\ -SubjectName 'CN=DSC.Company.Pri, OU=IT, DC=Company, DC=Pri' -Verbose}
+    
+# Can Export to PFX if needed on other web servers for high availability - Get-Help *pfx*
+
+#>
+
+
+# First -- we need resource modules - on Authoring box and Target
+# xPSDesiredStateConfiguration
+# XWebAdministration
+
+Invoke-command -ComputerName Cli1, PullServer {Install-Module -Name xPSDesiredStateConfiguration -RequiredVersion 5.0.0.0 -force}
+Invoke-command -ComputerName Cli1, PullServer {Install-Module -Name xWebAdministration -RequiredVersion 1.15.0.0 -force}
+
+
+ISE .\1.PullServer.ps1
+
+Start-DscConfiguration -Path .\ -ComputerName PullServer.Company.Pri -Verbose -Wait
--- DSCPullServer/1.PullServer.ps1
+++ DSCPullServer/1.PullServer.ps1
@@ -1,31 +1,74 @@
-﻿Configuration AutoLab {
+﻿$ConfigData=@{
+    # Node specific data
+    AllNodes = @(
+
+       # All Servers need following identical information 
+       @{
+            NodeName           = '*'
+           # PSDscAllowPlainTextPassword = $true;
+           # PSDscAllowDomainUser = $true
+            
+       },
+
+       # Unique Data for each Role
+       @{
+            NodeName = 'PullServer.company.pri'
+            Role = @('Web')#, 'PullServer')
+           
+ #           PullServerEndPointName = 'PSDSCPullServer'
+ #           PullserverPort = 8080                      #< - ask me why I use this port
+ #           PullserverPhysicalPath = "$env:SystemDrive\inetpub\wwwroot\PSDSCPullServer"
+ #           PullserverModulePath = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Modules"
+ #           PullServerConfigurationPath = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Configuration"
+ #           PullServerThumbPrint = Invoke-Command -Computername 'Pullserver.company.pri' {Get-Childitem Cert:\LocalMachine\My | Where-Object {$_.FriendlyName -like "*wild*"} | Select-Object -ExpandProperty ThumbPrint}
+ #
+ #           ComplianceServerEndPointName = 'PSDSCComplianceServer'
+ #           ComplianceServerPort = 9080
+ #           ComplianceServerPhysicalPath = "$env:SystemDrive\inetpub\wwwroot\PSDSCComplianceServer"
+ #           ComplianceServerThumbPrint = 'AllowUnencryptedTraffic'
+        }
+
+
+    );
+} 
+###########################################################################################
+
+
+
+
+
+
+
+Configuration Pullserver {
 
 #region DSC Resources
     Import-DSCresource -ModuleName PSDesiredStateConfiguration,
         @{ModuleName="xPSDesiredStateConfiguration";ModuleVersion="5.0.0.0"},
-        @{ModuleName="xActiveDirectory";ModuleVersion="2.14.0.0"},
-        @{ModuleName="xComputerManagement";ModuleVersion="1.8.0.0"},
-        @{ModuleName="xNetworking";ModuleVersion="3.0.0.0"},
-        @{ModuleName="xDhcpServer";ModuleVersion="1.5.0.0"},
-        @{ModuleName='xWindowsUpdate';ModuleVersion = '2.5.0.0'},
-        @{ModuleName='xPendingReboot';ModuleVersion = '0.3.0.0'},
-        @{ModuleName='xADCSDeployment';ModuleVersion = '1.0.0.0'}
+        @{ModuleName="xWebAdministration";ModuleVersion="1.15.0.0"}
 
 #endregion
 #region All Nodes
-    node $NodeName {
+    Node $AllNodes.where{$_.Role -eq 'Web'}.NodeName {
+    
+
+    # Install the IIS role
+
 
         ## Hack to fix DependsOn with hypens "bug" :(
         foreach ($feature in @(
-                'DNS',                           
-                'AD-Domain-Services',
-                'RSAT-AD-Tools', 
-                'RSAT-AD-PowerShell',
-                'GPMC'
-                #For Gui, might like
-                #'RSAT-DNS-Server',                     
-                #'RSAT-AD-AdminCenter',
-                #'RSAT-ADDS-Tools'
+                'Web-Server',
+                'Web-Default-Doc',                           
+                'Web-HTTP-Errors',
+                'Web-Static-Content', 
+                'Web-Filtering',
+                'Web-Dir-Browsing',
+                'Web-Stat-Compression',
+                # Additional components to support pullserver application
+                'Web-Net-Ext45',
+                'Web-Asp-Net45',
+                'Web-ISAPI-Ext',
+                #For Gui Management of IIS
+                'Web-Mgmt-Service'
 
             )) {
             WindowsFeature $feature.Replace('-','') {
@@ -34,5 +77,33 @@
                 IncludeAllSubFeature = $False;
             }
         } #End foreach
+
+        
+        #### Enabling GUI management of IIS
+        Registry RemoteManagement { # Can set other custom settings inside this reg key
+
+            Key = 'HKLM:\SOFTWARE\Microsoft\WebManagement\Server'
+            ValueName = 'EnableRemoteManagement'
+            ValueType = 'Dword'
+            ValueData = '1'
+            DependsOn = @('[WindowsFeature]WebMgmtService')
+       }
+
+       Service StartWMSVC {
+
+            Name = 'WMSVC'
+            StartupType = 'Automatic'
+            State = 'Running'
+            DependsOn = '[Registry]RemoteManagement'
+
+       }
     }
 }
+
+
+
+
+
+
+
+PullServer -ConfigurationData $ConfigData -OutputPath .\
